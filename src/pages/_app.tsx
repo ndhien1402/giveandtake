b38035327597e09a9bda26662788845d510acffc
hiennd <hiennd.codetot@gmail.com>
2023-05-18 23:48:20 +0700

    Add nprogress package

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,6 +4,9 @@ import { ChakraProvider } from "@chakra-ui/react";
 import type { NextPage } from "next";
 import type { ReactElement, ReactNode } from "react";
 import { extendTheme } from "@chakra-ui/react";
+import "nprogress/nprogress.css";
+import NProgress from "nprogress";
+import Router from "next/router";
 
 type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
   getLayout?: (page: ReactElement) => ReactNode;
@@ -14,6 +17,16 @@ type AppPropsWithLayout = AppProps & {
 };
 
 export default function App({ Component, pageProps }: AppPropsWithLayout) {
+  Router.events.on("routeChangeStart", NProgress.start);
+  Router.events.on("routeChangeError", NProgress.done);
+  Router.events.on("routeChangeComplete", NProgress.done);
+  NProgress.configure({
+    template:
+      '<div class="bar !bg-rose-500 !border-b-[3px] !border-rose-500" role="bar"><div class="peg !bg-rose-500"></div></div>',
+    easing: "ease",
+    speed: 300,
+    minimum: 0.1,
+  });
   const theme = extendTheme({
     colors: {
       color: {
