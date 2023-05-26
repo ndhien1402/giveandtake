4cbcc6d4ae16e12b9adde5b77b611e710cf40a87
hiennd <hiennd.codetot@gmail.com>
2023-05-26 20:29:39 +0700

    Update

--- src/pages/profile/index.tsx
+++ src/pages/profile/index.tsx
@@ -1,11 +1,19 @@
 import { ReactNode } from "react";
 import UserLayout from "layouts/UserLayout";
 import { Avatar, Box } from "@chakra-ui/react";
+import Image from "next/image";
 
 const Profile = () => {
   return (
     <>
-      <div>profile</div>
+      <div className="relative w-full h-[50vh] rounded-md overflow-hidden">
+        <Image
+          className="object-cover"
+          src={"https://wallpaperaccess.com/full/393511.jpg"}
+          fill
+          alt="background"
+        />
+      </div>
     </>
   );
 };
